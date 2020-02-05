9fe353a9cef6fb58e16d120f414a1e24e4b5d0d8
Jacob Christensen <christensenjj@msoe.edu>
2020-02-05 15:37:06 +0000

    fixed permissions issues

--- iot_project.py
+++ iot_project.py
@@ -7,18 +7,18 @@ Jacob Christensen
 import socket
 
 discover = "operation: discover\r\n"
-capabilities = "operation: capabilities\r\nname: Jacob's Device\r\nresources: 2\r\n" +
-	       "switch1\r\ntype: switch\r\nlamp1\r\ntype: lamp\r\n"
+capabilities = "operation: capabilities\r\nname: Jacob's Device\r\nresources: 2\r\nswitch1\r\ntype: switch\r\nlamp1\r\ntype: lamp\r\n"
 
 # Send DISCOVER broadcast
 mydevice = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-mydevice.sendto(bytes(discover, 'utf-8'),('192.168.24.255', 4961)
+mydevice.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+mydevice.sendto(bytes(discover, 'utf-8'),('192.168.24.255', 4961))
 
-while True:
+while True :
 	data, addr = mydevice.recvfrom(1024)
-	data.decode('utf-8')
+	data = data.decode('utf-8')
 	# Split the response into a dictionary
-	data_in = dict(d.split(": ", 1), for d in data.split("\r\n"))
+	data_in = dict(d.split(": ", 1) for d in data.split("\r\n"))
 	if(data_in["operation"] == "acknowledge") :
 		# Respond to ACKNOWLEDGE message with RESPONSE
 		mydevice.sendto(capabilities, addr)
