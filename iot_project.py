b6519baf56a1658f2c44a9dd4b9064298706530d
Jacob Christensen <christensenjj@msoe.edu>
2020-02-10 15:32:10 +0000

    added GPIO library and setup

--- iot_project.py
+++ iot_project.py
@@ -5,9 +5,19 @@ Jacob Christensen
 """
 
 import socket
+import RPi.GPIO as GPIO
 
 discover = "operation: discover\r\n"
-capabilities = "operation: capabilities\r\nname: Jacob's Device\r\nresources: 2\r\nswitch1\r\ntype: switch\r\nlamp1\r\ntype: lamp\r\n"
+capabilities = "operation: capabilities\r\nname: Jacob's Device\r\nresources: 2\r\n[resource 1]\r\ntype: switch\r\n[resource 2]\r\ntype: lamp\r\n"
+
+# Setup GPIO Pins
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+inputs = [16, 19]
+GPIO.setup(inputs, GPIO.IN)
+outputs = [4, 6, 12, 13, 20, 21, 26]
+GPIO.setup(outputs, GPIO.OUT)
+GPIO.output(outputs, GPIO.LOW)
 
 # Send DISCOVER broadcast
 mydevice = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
